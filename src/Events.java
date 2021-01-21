df24fbac6e087fd39f52a49078cbbc4a5424ca37
AkshayPatel-360 <67767059+AkshayPatel-360@users.noreply.github.com>
2021-01-21 15:50:14 -0500

    Created multiple working buttons using Action listner

---
+++ src/Events.java
@@ -0,0 +1,77 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class Events  {
+
+    public int width ;
+    public int height;
+    public Button button1;
+    public Button button2;
+    public JFrame frame;
+    public JPanel panel;
+    public Label label1;
+    public Label label2;
+    public int age = 0;
+    public  int count = 0 ;
+
+
+
+    public Events()
+    {
+
+        frame = new JFrame();
+        panel = new JPanel();
+        label1 = new Label("Age : " + age);
+        label2 = new Label("Count : " + count);
+        button1 = new Button("Click Me for Age ");
+        button2 = new Button("Click Me for Count");
+        button1.addActionListener(listner1);
+        button2.addActionListener(listener2);
+
+
+        panel.setBorder(BorderFactory.createEmptyBorder(40,20,40,20));
+        panel.setLayout(new GridLayout(0,1));
+        panel.add(button1);
+        panel.add(label1);
+        panel.add(button2);
+        panel.add(label2);
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setTitle("Counter");
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+
+
+
+
+
+
+
+    }
+
+
+    ActionListener listner1 = new ActionListener()
+    {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            age++;
+            label1.setText("Age : " + age );
+        }
+    };
+
+    ActionListener listener2 = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            count++;
+            label2.setText("Count : " +count);
+        }
+    };
+
+
+}
--- src/TestingClass.java
+++ src/TestingClass.java
@@ -8,18 +8,19 @@ public class TestingClass {
 
     public static void main(String[] args)
     {
-        Colors colors ;
+       /* Colors colors ;
         Colors MyFavColour = Colors.Red;
         System.out.println( MyFavColour);
         System.out.println( Colors.valueOf("Red"));
         System.out.println(Colors.Green.ordinal());
         System.out.println(Arrays.toString(Colors.values()));
 
-        System.out.println(Colors.Green.getValue());
+        System.out.println(Colors.Green.getValue());*/
+
+        new Events();
+
+
     }
-
-
-
 
 
 }
